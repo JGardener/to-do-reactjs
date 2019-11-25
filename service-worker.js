1dbdf9f347bea42e6f8c93dacb541108a1e9c969
JGardener <jamesgardener92@gmail.com>
2019-11-25 12:09:08 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-reactjs/precache-manifest.a724c96bad2431e77ba5895cac69b53e.js"
+  "/to-do-reactjs/precache-manifest.fccd483a1c768f7294123ed56c24dff2.js"
 );
 
 self.addEventListener('message', (event) => {
