d4343e914356c5640c11d00fd169e7100d2df9a4
JGardener <jamesgardener92@gmail.com>
2019-11-28 15:02:32 +0000

    Added comments to removeItem functionality for revision purposes

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -19,7 +19,13 @@ const List = () => {
     }
   };
 
-  // ADD REMOVE ITEM FUNCTIONALITY
+  // This is where it got really complicated for me.
+  // I needed to make a second array so React knew to re-render.
+  // The original array was being modified, sure, but React didn't see the difference, I guess.
+
+  // newArray takes the changes to state: items
+  // we then update state:items to whatever is inside newArray
+  // Fun!
   const removeItem = item => {
     const index = items.findIndex(listItem => {
       return item.target.innerHTML === listItem.title;
